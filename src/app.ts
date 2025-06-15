7719c8cf9ecac7413260485b99b700a70b6d29dc
James Kiama <kiamajames29@gmail.com>
2025-06-16 00:04:44 +0300

    fix: new changes

--- src/app.ts
+++ src/app.ts
@@ -30,10 +30,10 @@ const BASE_PATH = config.BASE_PATH;
 app.use(helmet());
 
 // parse json request body
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
 
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 
 app.use(xssMiddleware());
 
